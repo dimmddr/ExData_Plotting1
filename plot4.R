f2a89b4c0f3c4e774598912bdb9a619bbb0118a7
dimmddr <dmitrius-i@ya.ru>
2015-05-10 23:40:58 +0300

    Add completed script Plot4.R

--- plot4.R
+++ plot4.R
@@ -1,6 +1,16 @@
 data <- read.table("household_power_consumption.txt", sep = ";", dec = ".", header = FALSE, 
-                   colClasses = c("Date", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), 
+                   colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), 
                    na.strings = "?", skip = 66637, nrows = 2880)
 headers <- read.table("household_power_consumption.txt", nrows = 1, sep = ";")
 colnames(data) <- unlist(headers)
-data$Time <- strptime(data$Time, format = "%T")
+datetime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+png("Plot4.png")
+par(mfrow = c(2, 2))
+plot(datetime, data$Global_active_power, type = "l",  ylab = "Global Active Power (kilowatts)", xlab = "")
+plot(datetime, data$Voltage, type = "l",  ylab = "Voltage")
+plot(datetime, data$Sub_metering_1, type = "l",  ylab = "Energy sub metering", xlab = "")
+lines(datetime, data$Sub_metering_2, col = "red")
+lines(datetime, data$Sub_metering_3, col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1, 1, 1))
+plot(datetime, data$Global_reactive_power, type = "l")
+dev.off()
