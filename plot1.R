292425fe0e653756c9c0ece7e87e1ea79178aba6
dimmddr <dmitrius-i@ya.ru>
2015-05-10 22:52:33 +0300

    Plot1 png create, Plot2 almost complete. Need Plot2 png

--- plot1.R
+++ plot1.R
@@ -1,7 +1,10 @@
 data <- read.table("household_power_consumption.txt", sep = ";", dec = ".", header = FALSE, 
-                   colClasses = c("Date", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), 
+                   colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), 
                    na.strings = "?", skip = 66637, nrows = 2880)
 headers <- read.table("household_power_consumption.txt", nrows = 1, sep = ";")
 colnames(data) <- unlist(headers)
 data$Time <- strptime(data$Time, format = "%T")
-hist(data$Global_active_power, col = "red")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+hist(data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.copy(png, file = "Plot1.png")
+dev.off()
