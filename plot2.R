9359b25abeeea9dcdca2956f9b78f0363794ebc6
dimmddr <dmitrius-i@ya.ru>
2015-05-10 23:32:59 +0300

    Plot3 complete. Change png creation to png func

--- plot2.R
+++ plot2.R
@@ -4,6 +4,6 @@ data <- read.table("household_power_consumption.txt", sep = ";", dec = ".", head
 headers <- read.table("household_power_consumption.txt", nrows = 1, sep = ";")
 colnames(data) <- unlist(headers)
 datetime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+png("Plot2.png")
 plot(datetime, data$Global_active_power, type = "l",  ylab = "Global Active Power (kilowatts)")
-dev.copy(png, file = "Plot2.png")
 dev.off()
